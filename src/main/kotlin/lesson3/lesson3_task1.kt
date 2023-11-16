d1d07f57740809b9add23c8faafe98a509c8b4fe
Ivan Tarasov <guser999@mail.ru>
2023-11-16 21:39:46 +0300

    ks-1-3 полёт в космос

--- src/main/kotlin/lesson3/lesson3_task1.kt
+++ src/main/kotlin/lesson3/lesson3_task1.kt
@@ -1,5 +1,13 @@
 package lesson3
 
 fun main() {
-    println("Start KotlinSprint. Lesson 3, task 1")
+    val year = "1961"
+    var hour = "09"
+    var minute = "07"
+    println(year)
+    println(hour)
+    println(minute)
+    hour = "10"
+    minute = "55"
+    println("$hour:$minute")
 }
