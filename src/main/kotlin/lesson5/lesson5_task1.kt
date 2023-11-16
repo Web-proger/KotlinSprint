12a5840045a41f5ae4b94727879e8b879fc9bde8
Ivan Tarasov <guser999@mail.ru>
2023-11-16 22:37:43 +0300

    ks-1-5 Время провёдённое в космосе

--- src/main/kotlin/lesson5/lesson5_task1.kt
+++ src/main/kotlin/lesson5/lesson5_task1.kt
@@ -1,5 +1,15 @@
 package lesson5
 
 fun main() {
-    println("Start KotlinSprint. Lesson 5, task 1")
+    // 1ч 48 мин = 108 мин = 6480 сек
+    val secondsInMinute = 60
+    val secondsInHour = secondsInMinute * 60
+
+    val flightTimeInSeconds = 6480
+    val hours = flightTimeInSeconds / secondsInHour
+    val minutes = (6480 - hours * secondsInHour) / secondsInMinute
+    val seconds = (6480 - hours * secondsInHour - minutes * secondsInMinute)
+
+    // Может есть более элегантный способ, но я нагуглил только через .padStart и .format
+    println("${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}")
 }
