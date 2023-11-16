126172d9b0ae47f25568c92f6c2e65eb2c592a6d
Ivan Tarasov <guser999@mail.ru>
2023-11-16 21:09:40 +0300

    ks-1-1 правильное именование переменных

--- src/main/kotlin/lesson1/lesson1_task1.kt
+++ src/main/kotlin/lesson1/lesson1_task1.kt
@@ -1,5 +1,9 @@
 package lesson1
 
 fun main() {
-    println("Start KotlinSprint. Lesson 1, task 1")
+    // Количество заказов может меняться, поэтому уместнее var, но c учётом отсутствия другой логики, оставил val
+    val orderCount = 75
+    val orderSuccessMessage = "Поздравляем с успешной покупкой"
+    println("Сегодня в магазине было оформлено $orderCount заказов")
+    println(orderSuccessMessage)
 }
