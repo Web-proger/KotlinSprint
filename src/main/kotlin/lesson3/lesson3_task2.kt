a4024c703ed09abe2146a4c7a27822a63b98d346
Ivan Tarasov <guser999@mail.ru>
2023-11-17 23:33:52 +0300

    KS-3-2 смена фамилии

---
+++ src/main/kotlin/lesson3/lesson3_task2.kt
@@ -0,0 +1,13 @@
+package lesson3
+
+fun main() {
+    val maidenName = "Андреева"
+    val name = "Татьяна"
+    val patronymic = "Сергеевна"
+    val surname = "Сидорова"
+    val ageBeforeMarriage = 20
+    val ageAtMarriage = 22
+
+    println("[$maidenName $name $patronymic, $ageBeforeMarriage]")
+    println("[$surname $name $patronymic, $ageAtMarriage]")
+}
