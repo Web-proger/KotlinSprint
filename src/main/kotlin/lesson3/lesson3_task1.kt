e30be3e46a6c4812b0e1c7187f5b688b38c22474
Ivan Tarasov <guser999@mail.ru>
2023-11-17 23:31:04 +0300

    KS-3-1 вариации приветствия

---
+++ src/main/kotlin/lesson3/lesson3_task1.kt
@@ -0,0 +1,10 @@
+package lesson3
+
+fun main() {
+    val userName = "Степан"
+    val dayWelcomeMessage = "Доброго дня, $userName!"
+    val nightWelcomeMessage = "Доброго вечера, $userName!"
+
+    println(dayWelcomeMessage)
+    println(nightWelcomeMessage)
+}
