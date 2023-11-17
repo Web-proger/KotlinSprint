6de7240bee3242215681823afcd666bb41f637c4
Ivan Tarasov <guser999@mail.ru>
2023-11-17 16:41:17 +0300

    ks-1-5 правки по ревью. padStart => .format

--- src/main/kotlin/lesson1/lesson1_task5.kt
+++ src/main/kotlin/lesson1/lesson1_task5.kt
@@ -1,15 +1,13 @@
 package lesson1
 
 fun main() {
-    // 1ч 48 мин = 108 мин = 6480 сек
     val secondsInMinute = 60
     val secondsInHour = secondsInMinute * 60
-
     val flightTimeInSeconds = 6480
+
     val hours = flightTimeInSeconds / secondsInHour
     val minutes = (6480 - hours * secondsInHour) / secondsInMinute
     val seconds = (6480 - hours * secondsInHour - minutes * secondsInMinute)
 
-    // Может есть более элегантный способ, но я нагуглил только через .padStart и .format
-    println("${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}")
+    println(String.format("%02d:%02d:%02d", hours, minutes, seconds))
 }
