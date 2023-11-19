879a308dafd5421bd8627a4e3008338185b718bb
Ivan Tarasov <guser999@mail.ru>
2023-11-19 13:21:08 +0300

    KS-4-2 правки по ревью. Часть вводных в константы.

--- src/main/kotlin/lesson4/lesson4_task2.kt
+++ src/main/kotlin/lesson4/lesson4_task2.kt
@@ -1,17 +1,17 @@
 package lesson4
 
-fun main() {
-    val averageMinimumWeightInKg = 35
-    val averageMaximumWeightInKg = 100
-    val maximumVolumeInLiters = 100
+const val AVERAGE_MINIMUM_WEIGHT_IN_KG = 35
+const val AVERAGE_MAXIMUM_WEIGHT_IN_KG = 100
+const val AVERAGE_MAXIMUM_VOLUME_IN_LITERS = 100
 
+fun main() {
     val cargo1Weight = 20
     val cargo1Volume = 80
     val cargo2Weight = 50
     val cargo2Volume = 100
 
     println("Груз с весом $cargo1Weight кг и объемом $cargo1Volume л соответствует категории 'Average': " +
-            "${(cargo1Weight >= averageMinimumWeightInKg && cargo1Weight <= averageMaximumWeightInKg) && cargo1Volume < maximumVolumeInLiters }")
+            "${(cargo1Weight >= AVERAGE_MINIMUM_WEIGHT_IN_KG && cargo1Weight <= AVERAGE_MAXIMUM_WEIGHT_IN_KG) && cargo1Volume < AVERAGE_MAXIMUM_VOLUME_IN_LITERS }")
     println("Груз с весом $cargo2Weight кг и объемом $cargo2Volume л соответствует категории 'Average': " +
-            "${(cargo2Weight >= averageMinimumWeightInKg && cargo2Weight <= averageMaximumWeightInKg) && cargo2Volume < maximumVolumeInLiters }")
+            "${(cargo2Weight >= AVERAGE_MINIMUM_WEIGHT_IN_KG && cargo2Weight <= AVERAGE_MAXIMUM_WEIGHT_IN_KG) && cargo2Volume < AVERAGE_MAXIMUM_VOLUME_IN_LITERS }")
 }
