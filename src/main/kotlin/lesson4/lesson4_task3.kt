a6f89de3c7e3adc0a2491464e2d9d49c41f59494
Ivan Tarasov <guser999@mail.ru>
2023-11-20 10:27:55 +0300

    KS-4-3 правки по ревью. Неизменные данные вынес в контсанты

--- src/main/kotlin/lesson4/lesson4_task3.kt
+++ src/main/kotlin/lesson4/lesson4_task3.kt
@@ -1,20 +1,20 @@
 package lesson4
 
-fun main() {
-    val isSunnyWeather = true
-    val isOpenTent = true
-    val airHumidity = 20
-    val excludeTheSeason = "winter"
+val IS_SUNNY_WEATHER = true
+val IS_OPEN_TENT = true
+val AIR_HUMIDITY = 20
+val EXCLUDE_THE_SEASON = "winter"
 
+fun main() {
     val isCurrentSunnyWeather = true
     val isCurrentOpenTent = true
     val currentAirHumidity = 20
     val currentSeason = "winter"
 
-    val isFavorableEnvironment = isCurrentSunnyWeather == isSunnyWeather
-        && isOpenTent == isCurrentOpenTent
-        && airHumidity == currentAirHumidity
-        && currentSeason != excludeTheSeason
+    val isFavorableEnvironment = isCurrentSunnyWeather == IS_SUNNY_WEATHER
+        && IS_OPEN_TENT == isCurrentOpenTent
+        && AIR_HUMIDITY == currentAirHumidity
+        && currentSeason != EXCLUDE_THE_SEASON
 
     println("Благоприятные ли условия сейчас для роста бобовых? $isFavorableEnvironment")
 }
